923e02ad98091bee16fda9c9719f0dc832abce3a
Mehull8 <100364966+Mehull8@users.noreply.github.com>
2022-05-12 20:33:10 +0530

    Add files via upload

---
+++ assignment 3 output.py
@@ -0,0 +1,58 @@
+Python 3.10.4 (v3.10.4:9d38120e33, Mar 23 2022, 17:29:05) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======================================================================== RESTART: /Users/mehulsethi/Downloads/assignment 3.py ========================================================================
+Q1
+Enter a number
+======================================================================== RESTART: /Users/mehulsethi/Downloads/assignment 3.py ========================================================================
+Q1
+Enter a number:31
+Binary equivalent of 31 is 11111
+Q2
+Enter expression:5-8+(44/4)**2
+5-8+(44/4)**2 = 118.0
+Q3
+Enter an integer:3
+a) 35
+b) 3.0
+enter radius:6
+c) 452.3893421169302
+Enter angle 1:30
+Enter angle 1:50
+d) 0.7455229318347486
+Enter x coordinate of first point:3
+Enter y coordinate of first point:7
+Enter x coordinate of second point:1
+Enter y coordinate of second point:6
+e) 0.5
+Q4
+4.(a)
+0
+1
+2
+3
+4
+4.(b)
+3
+4
+5
+6
+7
+8
+9
+4.(c)
+4
+7
+10
+4.(d)
+15
+13
+11
+9
+7
+4.(e)
+Q5
+Enter number of carbon atoms in molecule:2
+Enter number of hydrogen atoms in molecule:4
+Enter number of oxygen atoms in molecule:2
+The molecular weight of the compound is 60.051959999999994
