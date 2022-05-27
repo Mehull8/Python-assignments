ed6c2487644deb7632e376fe6788eeeb04ef7713
Mehull8 <100364966+Mehull8@users.noreply.github.com>
2022-05-27 23:04:18 +0530

    Add files via upload

---
+++ assignment 4 output.py
@@ -0,0 +1,30 @@
+Q1
+Enter mark:54
+Grade: C
+Continue?(y/n)n
+Q2
+Enter year:2020
+Leap year
+Q3
+Question 1:8x6=65
+Wrong. The correct answer is 48
+Question 2:2x7=4
+Wrong. The correct answer is 14
+Question 3:1x4=22
+Wrong. The correct answer is 4
+Question 4:9x1=9
+Right!
+Question 5:2x2=4
+Right!
+Question 6:0x9=9
+Wrong. The correct answer is 0
+Question 7:6x1=6
+Right!
+Question 8:5x3=15
+Right!
+Question 9:3x2=6
+Right!
+Question 10:6x3=18
+Right!
+Q4
+Answer: 177
